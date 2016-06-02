af5647a4ba073f6ba4ed74b8501c13783ed61b9d
machunmei <chunmei.ma@careerbuilder.com>
2016-06-02 19:54:38 +0800

    test

---
+++ contract-edit.js
@@ -0,0 +1,6 @@
+angular.module("app", []);
+
+angular.module("app").controller("contractEditCtrl", ["$scope",'$http', function ($scope,$http) {
+     $scope.name = "cma";
+
+}]);
---
+++ contract-list.js
@@ -0,0 +1,3 @@
+angular.module("app").controller("contractListCtrl", ["$scope",'$http', function ($scope,$http) {
+
+}]);
